43a533b6c7d60d527a7482bfb58e6512a88c7392
winniesi <si.good@gmail.com>
2022-02-14 13:27:36 +0800

    config

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '4453c87263134959b8a7c215d03d1ed6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: '一些记录',
   domain: 'transitivebullsh.it',
   author: 'Travis Fischer',
 
@@ -17,7 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  twitter: 'codemorningsir',
   github: 'transitive-bullshit',
   linkedin: 'fisch2',
 
